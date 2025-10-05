bd4ab50968a79bc5e694ec653486e35040cb86be
Keyman Luxy Sackey <sackeykeyman@gmail.com>
2025-10-05 21:21:21 +0100

    Created a to-do list

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function (){
+    const addButton = document.getElementById("add-task-button");
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
+
+    function addTask(){
+        let taskText = document.getElementById("task-input").value.trim();
+
+        //task creation and Removal
+        if (taskTest === ""){
+            alert("Please enter a task");
+        }
+            let li = document.createElement("li");
+            li.textContent = taskTest;
+
+           const button = document.createElement("button");
+button.textContent = "Remove";
+button.classList.add("remove-btn");
+
+button.addEventListener("click", function() {
+  li.remove();
+});
+
+            li.append(button);
+            taskList.append(li);
+            
+            taskInput.value ="";
+}
+            //Attaching event listeners
+            addButton.addEventListener("click", addTask);
+            taskInput.addEventListener("keypress", function(event){
+                if (event.key === "Enter"){
+                    addTask();
+                }
+            })
+})
