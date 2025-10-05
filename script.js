c602df7c0c04b45d8046c81b182750f7f07719c0
Keyman Luxy Sackey <sackeykeyman@gmail.com>
2025-10-05 21:28:37 +0100

    Fixed Bug

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function (){
-    const addButton = document.getElementById("Add Task");
+    const addButton = document.getElementById("add-task-btn");
     const taskInput = document.getElementById("task-input");
     const taskList = document.getElementById("task-list");
 
